bacb0a00691e964fc52534f251a28c77e19245e0
Carson Reinke <carson@reinke.co>
2023-11-19 08:27:43 -0500

    Add skipped stats

--- downloader/stats.go
+++ downloader/stats.go
@@ -6,15 +6,16 @@ import (
 
 // Stats TODO
 type Stats struct {
-	Total int
-	Errors int
-	TotalSize uint64
+	Total      int
+	Errors     int
+	TotalSize  uint64
 	Downloaded int
+	Skipped    int
 
 	mutex sync.Mutex
 }
 
-// UpdateStatsTotal TODO
+// UpdateStatsTotal increment the total items
 func (s *Stats) UpdateStatsTotal(total int) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
@@ -22,7 +23,7 @@ func (s *Stats) UpdateStatsTotal(total int) {
 	s.Total += total
 }
 
-// UpdateStatsDownloaded TODO
+// UpdateStatsDownloaded increment the downloaded items and size of items
 func (s *Stats) UpdateStatsDownloaded(totalSize uint64, downloaded int) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
@@ -31,10 +32,18 @@ func (s *Stats) UpdateStatsDownloaded(totalSize uint64, downloaded int) {
 	s.Downloaded += downloaded
 }
 
-// UpdateStatsError TODO
+// UpdateStatsError increment the items that produced errors
 func (s *Stats) UpdateStatsError(errors int) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
 	s.Errors += errors
+}
+
+// UpdateStatsError increment the skipped items
+func (s *Stats) UpdateStatsSkipped(skipped int) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.Skipped += skipped
 }
